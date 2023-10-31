bddd435b9be481e415f0aa3649439513a5398263
tarin <tarinakter567@gmail.com>
2023-10-31 22:55:49 +0600

    Initial commit

---
+++ lib/fullName.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+class FullName extends StatefulWidget {
+  const FullName({super.key});
+
+  @override
+  State<FullName> createState() => _FullNameState();
+}
+
+class _FullNameState extends State<FullName> {
+  String fullName ='';
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Full Name Show'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: firstName,
+          ),
+          TextField(
+            controller: lastName,
+          ),
+          TextButton(
+            onPressed: (){
+              setState(() {
+                fullName = "${firstName.text} ${lastName.text}";
+              });
+            },
+          child: Text('Add')
+          ),
+          Text('Full Name is $fullName'),
+        ],
+      ),
+    );
+  }
+}
